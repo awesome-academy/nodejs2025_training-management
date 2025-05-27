nthanhminh/Training-management-system

handle course searching for supervisor

src/modules/courses/course.controller.ts
@@ -1,63 +1,115 @@
-import { Body, Controller, Delete, Param, Patch, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { CreateCourseDto } from "./dto/createCourse.dto";
-import { AppResponse } from "src/types/common.type";
-import { CourseService } from "./course.service";
-import { Course } from "./entity/course.entity";
-import { CurrentUserDecorator } from "src/decorators/current-user.decorator";
-import { User } from "@modules/users/entity/user.entity";
-import { UpdateCourseDto } from "./dto/updateCourse.dto";
-import { UpdateResult } from "typeorm";
-import { UpdateSubjectForCourseDto } from "./dto/UpdateSubjectForTask.dto";
-import { CourseSubject } from "@modules/course_subject/entity/course_subject.entity";
-import { DeleteSubjectCourseDto } from "./dto/deleteSubject.dto";
-import { EmailDto } from "src/common/dto/email.dto";
-import { SupervisorCourse } from "@modules/supervisor_course/entity/supervisor_course.entity";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateCourseDto } from './dto/createCourse.dto';
+import { AppResponse } from 'src/types/common.type';
+import { CourseService } from './course.service';
+import { Course } from './entity/course.entity';
+import { CurrentUserDecorator } from 'src/decorators/current-user.decorator';
+import { User } from '@modules/users/entity/user.entity';
+import { UpdateCourseDto } from './dto/updateCourse.dto';
+import { UpdateResult } from 'typeorm';
+import { UpdateSubjectForCourseDto } from './dto/UpdateSubjectForTask.dto';
+import { CourseSubject } from '@modules/course_subject/entity/course_subject.entity';
+import { DeleteSubjectCourseDto } from './dto/deleteSubject.dto';
+import { EmailDto } from 'src/common/dto/email.dto';
+import { SupervisorCourse } from '@modules/supervisor_course/entity/supervisor_course.entity';
+import { FindCourseDto } from './dto/findCourse.dto';
+import { RemoveCreatorInterceptor } from 'src/interceptors/removeCreator.interceptor';
 
 @Controller('courses')
 @ApiTags('courses')
+@UseInterceptors(RemoveCreatorInterceptor)
 export class CourseController {
-    constructor(
-        private readonly courseService: CourseService
-    ) {}
-    @Post()
-    async createCourse(@Body() dto: CreateCourseDto, @CurrentUserDecorator() user: User) : Promise<AppResponse<Course>> {
+    constructor(private readonly courseService: CourseService) {}
+
+    @Get('supervisor')
+    async getCourseBySupervisor(
+        @Query() dto: FindCourseDto,
+            @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<Course[]>> {
         return {
-            data: await this.courseService.createNewCourse(dto, user)
-        }
+            data: await this.courseService.supervisorFindCourse(dto, user),
+        };
+    }
+
+    @Get('supervisor/:courseId')
+    async getCourseDetailBySupervisor(
+        @Param('courseId') courseId: string,
+            @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<Course>> {
+        return {
+            data: await this.courseService.getCourseDetailForSupervisor(courseId, user),
+        };
+    }
+
+    @Post()
+    async createCourse(@Body() dto: CreateCourseDto, @CurrentUserDecorator() user: User): Promise<AppResponse<Course>> {
+        return {
+            data: await this.courseService.createNewCourse(dto, user),
+        };
     }
 
     @Patch('subject/:id')
-    async updateCourse(@Body() dto: UpdateSubjectForCourseDto, @CurrentUserDecorator() user: User, @Param('id') id: string) : Promise<AppResponse<CourseSubject[]>> {
+    async updateCourse(
+        @Body() dto: UpdateSubjectForCourseDto,
+        @CurrentUserDecorator() user: User,
+        @Param('id') id: string,
+    ): Promise<AppResponse<CourseSubject[]>> {
+        console.log(dto, user, id);
         return {
-            data: await this.courseService.updateSubjectForCourse(dto, user, id)
-        }
+            data: await this.courseService.updateSubjectForCourse(dto, user, id),
+        };
     }
+
+    // async deletSubjectForCouse(
+    // @Body() dto: UpdateSubjectForCourseDto,
+    //     @CurrentUserDecorator() user: User,
+    //     @Param('id') id: string,
+    // ) {
+    //     return {
+    //         data:
+    //     }
+    // }
     @Patch('info/:id')
-    async updateSubjectCourse(@Body() dto: UpdateCourseDto, @CurrentUserDecorator() user: User, @Param('id') id: string) : Promise<AppResponse<UpdateResult>> {
+    async updateSubjectCourse(
+        @Body() dto: UpdateCourseDto,
+            @CurrentUserDecorator() user: User,
+            @Param('id') id: string,
+    ): Promise<AppResponse<UpdateResult>> {
         return {
-            data: await this.courseService.updateCourseInfo(dto, user, id)
-        }
+            data: await this.courseService.updateCourseInfo(dto, user, id),
+        };
     }
 
     @Patch('supervisor/:id')
-    async addNewSupervisor(@Body() dto: EmailDto, @CurrentUserDecorator() user: User, @Param('id') id: string) : Promise<AppResponse<SupervisorCourse>> {
+    async addNewSupervisor(
+        @Body() dto: EmailDto,
+            @CurrentUserDecorator() user: User,
+            @Param('id') id: string,
+    ): Promise<AppResponse<SupervisorCourse>> {
         return {
-            data: await this.courseService.addSupervisor(dto, id, user)
-        }
+            data: await this.courseService.addSupervisor(dto, id, user),
+        };
     }
 
     @Delete(':id')
-    async deleteOneCourse(@Param('id') id: string, @CurrentUserDecorator() user: User) : Promise<AppResponse<UpdateResult>> {
+    async deleteOneCourse(
+        @Param('id') id: string,
+            @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<UpdateResult>> {
         return {
-            data: await this.courseService.deleteCourse(id, user)
-        }
+            data: await this.courseService.deleteCourse(id, user),
+        };
     }
 
     @Delete('sub/:id')
-    async deleteSubjectCourse(@Param('id') id: string, @Body() dto: DeleteSubjectCourseDto, @CurrentUserDecorator() user: User) : Promise<AppResponse<UpdateResult>> {
+    async deleteSubjectCourse(
+        @Param('id') id: string,
+            @Body() dto: DeleteSubjectCourseDto,
+            @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<UpdateResult>> {
         return {
-            data: await this.courseService.deleteSubjectForCourse(id, dto, user)
-        }
+            data: await this.courseService.deleteSubjectForCourse(id, dto, user),
+        };
     }
 }

awesome-academy/nodejs2025_training-management

src/modules/courses/course.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Delete, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    ClassSerializerInterceptor,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+    UseGuards,
+    UseInterceptors,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateCourseDto } from './dto/createCourse.dto';
 import { AppResponse } from 'src/types/common.type';
@@ -13,10 +25,12 @@ import { CourseSubject } from '@modules/course_subject/entity/course_subject.ent
 import { DeleteSubjectCourseDto } from './dto/deleteSubject.dto';
 import { EmailDto } from 'src/common/dto/email.dto';
 import { SupervisorCourse } from '@modules/supervisor_course/entity/supervisor_course.entity';
-import { Roles } from 'src/decorators/roles.decorator';
-import { ERolesUser } from '@modules/users/enums/index.enum';
-import { SessionAuthGuard } from '@modules/auth/guards/session.guard';
+import { FindCourseDto } from './dto/findCourse.dto';
 import { RolesGuard } from '@modules/auth/guards/roles.guard';
+import { SessionAuthGuard } from '@modules/auth/guards/session.guard';
+import { ERolesUser } from '@modules/users/enums/index.enum';
+import { Roles } from 'src/decorators/roles.decorator';
+import { CourseWithoutCreatorDto } from './responseDto/courseResponse.dto';
 
 @Controller('courses')
 @ApiTags('courses')
@@ -25,8 +39,36 @@ export class CourseController {
 
     @Roles(ERolesUser.SUPERVISOR)
     @UseGuards(SessionAuthGuard, RolesGuard)
+    @Get('supervisor')
+    async getCourseBySupervisor(
+        @Query() dto: FindCourseDto,
+        @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<Course[]>> {
+        return {
+            data: await this.courseService.supervisorFindCourse(dto, user),
+        };
+    }
+
+    @Roles(ERolesUser.SUPERVISOR)
+    @UseGuards(SessionAuthGuard, RolesGuard)
+    @Get('supervisor/:courseId')
+    async getCourseDetailBySupervisor(
+        @Param('courseId') courseId: string,
+        @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<Course>> {
+        return {
+            data: await this.courseService.getCourseDetailForSupervisor(courseId, user),
+        };
+    }
+
+    @Roles(ERolesUser.SUPERVISOR)
+    @UseGuards(SessionAuthGuard, RolesGuard)
+    @UseInterceptors(ClassSerializerInterceptor)
     @Post()
-    async createCourse(@Body() dto: CreateCourseDto, @CurrentUserDecorator() user: User): Promise<AppResponse<Course>> {
+    async createCourse(
+        @Body() dto: CreateCourseDto,
+        @CurrentUserDecorator() user: User,
+    ): Promise<AppResponse<CourseWithoutCreatorDto>> {
         return await this.courseService.createNewCourse(dto, user);
     }
 